8c0ae19653938cd663c687b363a243f374c1b55d
Herman Stander <her.stander@gmail.com>
2022-07-01 13:40:25 +0200

    added evn for movie

--- src/components/Map.js
+++ src/components/Map.js
@@ -212,6 +212,7 @@ function Map({ organisation: {config, id, name} }) {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
+              
               <Marker position={[51.505, -0.09]}>
                 <Popup>
                   A pretty CSS3 popup. <br /> Easily customizable.
